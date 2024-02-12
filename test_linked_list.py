afa3991e2de62ee676ea52766afc7fa7d2824b79
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-12 16:14:00 +0100

    removed comments

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,33 +1,31 @@
 import pytest
 from linked_list import Node, LinkedList
 
-# Test Node class
+
 @pytest.mark.parametrize("data, expected_data", [(5, 5), (10, 10)])
 def test_node(data, expected_data, capsys):
-    # Create a node with the provided data
     node = Node(data)
-    # Check if the data in the node matches the expected data
     assert node.get_data() == expected_data
-    # Check if the next node is None
     assert node.get_next() is None
 
-    # Capture the console output by calling the log method
     with capsys.disabled():
         node.log()
-    # Retrieve the captured output
     captured = capsys.readouterr()
-    # Print the captured output
     print(captured.out)
 
-# Test LinkedList class
-@pytest.mark.parametrize("nodes, expected_first_data, expected_prev_to_last_data, expected_last_data", [
-    ([], None, None, None),
-    ([Node(5)], 5, None, 5),
-    ([1, 2], 1, 1, 2),
-    ([1, 2, 3, 4, 5], 1, 4, 5)
-])
-def test_linked_list(nodes, expected_first_data, expected_prev_to_last_data, expected_last_data, capsys):
-    # Create a linked list with the provided nodes
+
+@pytest.mark.parametrize(
+    "nodes, expected_first_data, expected_prev_to_last_data, expected_last_data",
+    [
+        ([], None, None, None),
+        ([Node(5)], 5, None, 5),
+        ([1, 2], 1, 1, 2),
+        ([1, 2, 3, 4, 5], 1, 4, 5),
+    ],
+)
+def test_linked_list(
+    nodes, expected_first_data, expected_prev_to_last_data, expected_last_data, capsys
+):
     linked_list = LinkedList()
     for node in nodes:
         linked_list.push(node)
@@ -38,7 +36,9 @@ def test_linked_list(nodes, expected_first_data, expected_prev_to_last_data, exp
         assert expected_first_data is None
 
     try:
-        assert linked_list.get_previoust_to_last().get_data() == expected_prev_to_last_data
+        assert (
+            linked_list.get_previoust_to_last().get_data() == expected_prev_to_last_data
+        )
     except AttributeError:
         assert expected_prev_to_last_data is None
 
@@ -47,17 +47,14 @@ def test_linked_list(nodes, expected_first_data, expected_prev_to_last_data, exp
     except AttributeError:
         assert expected_last_data is None
 
-    # Capture the console output by calling the log method
     with capsys.disabled():
         linked_list.log()
-    # Retrieve the captured output
     captured = capsys.readouterr()
-    # Print the captured output
     print(captured.out)
 
-@pytest.mark.parametrize("nodes", [[],[1],[1,2],[1,2,3]])
+
+@pytest.mark.parametrize("nodes", [[], [1], [1, 2], [1, 2, 3]])
 def test_pop(nodes, capsys):
-    # Create a linked list with the provided nodes
     linked_list = LinkedList()
     for node in nodes:
         linked_list.push(node)
@@ -69,14 +66,11 @@ def test_pop(nodes, capsys):
     except AttributeError:
         assert linked_list.get_first() is None
 
-    # Capture the console output by calling the log method
     with capsys.disabled():
         linked_list.log()
-    # Retrieve the captured output
     captured = capsys.readouterr()
-    # Print the captured output
     print(captured.out)
 
+
 if __name__ == "__main__":
-    # Run the test cases using pytest
     pytest.main()
